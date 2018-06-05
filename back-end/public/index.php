49490ec33ce7dec890fa1dab9a6605182232e478
Paulo Jose Moreira <paulojosemoreira@gmail.com>
2018-06-05 00:47:41 -0300

    fix

--- back-end/public/index.php
+++ back-end/public/index.php
@@ -1,4 +1,21 @@
 <?php
+
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']) && (
+            $_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'] == 'POST' ||
+            $_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'] == 'DELETE' ||
+            $_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'] == 'PUT' )) {
+        header('Access-Control-Allow-Origin: http://vps8011.publiccloud.com.br');
+        header("Access-Control-Allow-Credentials: true");
+        header('Access-Control-Allow-Headers: X-Requested-With');
+        header('Access-Control-Allow-Headers: Content-Type');
+        header('Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PUT'); // http://stackoverflow.com/a/7605119/578667
+        header('Access-Control-Max-Age: 86400');
+    }
+    exit;
+}
+
+
 use Zend\Mvc\Application;
 use Zend\Stdlib\ArrayUtils;
 
